fa892dc87c7799083a150bd64ad90568e2785153
Akshatha Nayak <akshanayaki@yahoo.com>
2017-01-27 15:08:40 +0530

    Create TEST.cpp

---
+++ TEST.cpp
@@ -0,0 +1,22 @@
+//Life, the Universe, and Everything
+//solved on 10-04-16
+
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	int n;
+	start:
+	scanf("%d\n",&n);
+	if (n>=0&&n!=42)
+	{
+		printf("%d\n",n);
+		goto start;
+	}
+	else
+	{
+		goto stop;
+	}
+	stop:
+	return 0;
+}
